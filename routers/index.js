804a2f8d287625174e2213cae8e338f028452654
gaobowen <gaobowen@baidu.com>
2018-01-06 11:14:23 +0800

    1. update router/index.js

--- routers/index.js
+++ routers/index.js
@@ -10,7 +10,7 @@ module.exports = () => {
     const router = new Router();
     const fs = require("fs");
     router.get("/", (ctx, next) => {
-        ctx.body = 'hello world new' + Date.now();
+        ctx.body = 'hello world.' + (new Date);
     });
 
     router.get('/test', async(ctx, next) => {
